3b56efad15f379f2a11b2599d33eb445ab7e11f6
Ahmed Henine <ah01567@gmail.com>
2024-06-19 18:52:38 -0400

    removed the thereProvider Console log from the app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // Example primary color
+    },
+    secondary: {
+      main: '#dc004e', // Example secondary color
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
